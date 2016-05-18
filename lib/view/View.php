31b5314f8e9fe31d7d376a138c65f94f309a6c23
dellpinto <dellpinto@Inspirit>
2016-05-18 15:59:45 +0100

    View skeleton

--- lib/view/View.php
+++ lib/view/View.php
@@ -2,119 +2,72 @@
 
 namespace lib\view;
 
-use \lib\view\ParseTemplate;
-use \lib\view\StringTemplate;
-
 /**
  * Description of View
+ * A interface between Control and View
  *
- * the ordered methods are called in Controller
- * construct -> getExtended -> parse -> set -> display
- *
- * created in 14/nov/2014
- * @author Luís Pinto - luis.nestesitio@gmail.com
+ * @author Luís Pinto / luis.nestesitio@gmail.com
+ * Created @May 18, 2016
  */
-class View
-{
-    /**
-     * @var \lib\view\StringTemplate
-     */
-    private $template;
-
-     /**
-     * Create the obect ParseTemplate
-     * The first roun of included tag will be processed  in class ParseTemplate
-     * {% include ('Home/view/include.htm') %}
-      *
-     * @param String $string The path of the template file
-     *
-     * @return void
-     */
-    public function __construct($string, $file = true)
-    {
-        if ($file == true) {
-            /* Template process extends and css / js extensions */
-            $this->template = new ParseTemplate($string);
-            /*
-             * The first round of included tag will be processed
-             * {% include ('Home/view/include.htm') %}
-             */
-        } else {
-            $this->template = new StringTemplate($string);
-        }
-    }
+class View {
 
     /**
-     * @return mixed
+     * Define a class property to store the template 
+     * @var object
      */
-    public function getExtend()
-    {
-        return $this->template->getExtends();
-    }
-
+    private $view;
+    
     /**
-     * look for tags and functions in template
-     * {% while (item in list) %} ... {% endwhile %}
-     * {% if (status='open') %} ... {% elseif (status='ok') %} ... {% else %} ... {% endif %}
-     * {% embed ('Core\Menu::nav' @'layout/modern-business/nav.htm') %}
-     * Create array of tags and contents
-     *
-     * @return void
+     * Define a class property to store the entries 
+     * @var object
      */
-    public function parse()
-    {
-        $this->template->findTag('block');
-        $this->template->setIncludes();
-        $this->template->findTag('while');
-        $this->template->findTag('embed');
-        $this->template->findTag('if');
-        $this->template->findTag('attr');
+    private $data;
+    
+    /*
+     * Public method to output the result of the templating engine 
+     */
+    public function render(){
+        
     }
+    
 
+    function __construct($file) {
+        $this->loadTemplate($file);
+        $this->parseTemplate();
+    }
+    
     /**
-     * Building the array of pairs tag => data
-     * where data can be an array, string, int or empty
-     * @param String $tag The tag to be replaced {{ tag }}
-     * @param mixed $data The data to replace tag
-     *
-     * @return void
+     * Method to load the template
+     * 
+     * @param string $file
      */
-    public function set($tag, $data = '')
-    {
-        $this->template->set($tag, $data);
-
+    private function loadTemplate($file){
+        
     }
-
+    
     /**
-     * Process all the template, replacing tags for data
-     *
-     * @return String $this->template->getOutput() the completed html
+     * Method to parse the template
      */
-    public function display()
-    {
-        $this->template->parseAllPortions();
-        $this->template->parseTags();
-        return $this->template->getOutput();
+    private function parseTemplate(){
+        
     }
-
-    #other methods
-
+    
     /**
-     * Get the complete HTML
-     *
-     * @return String $this->template->getOutput() the completed html
+     * Method to replace the template tags with entry data
+     * 
+     * 
+     * @param type $tag
+     * @param type $data
+     * @return \lib\control\View
      */
-    public function getOutput()
+        public function set($tag, $data)
     {
-        return $this->template->getOutput();
+       
+            return $this;
     }
+    
 
-    /**
-     * @param $html
-     */
-    public function setOutput($html)
-    {
-        $this->template->setOutput($html);
-    }
+  
+  
 
 }
