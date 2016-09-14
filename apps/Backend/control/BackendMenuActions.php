17d9e48010b815db553b01e9e83c0331963b28ab
dellpinto <dellpinto@Inspirit>
2016-09-14 18:02:01 +0100

    Menu class for Backend

--- apps/Backend/control/BackendMenuActions.php
+++ apps/Backend/control/BackendMenuActions.php
@@ -23,32 +23,5 @@ class BackendMenuActions extends \lib\control\Controller {
     }
 
 
-    /**
-     *
-     */
-    public function navbackendAction(){
-        $links = $mainlinks = [];
-        $this->set('nav_home', UrlHref::renderUrl('/'));
-        $this->set('nav_backend', UrlHref::renderMenuUrl(['app'=>'backend'], 'Backend'));
-        $apps = \apps\Configs\model\AppsQuery::getAppsAccess();
-        $pages = \lib\page\HtmPageQueries::getBackendPages()->find();
-        foreach($pages as $page){
-            $btn = UrlHref::renderMenuUrl(['app'=>$page->getHtm()->getHtmApp()->getSlug(),'canonical'=>$page->getSlug()], $page->getMenu());
-            $links[$page->getHtm()->getHtmApp()->getSlug()][] = $btn;
-        }
-        foreach($apps as $app){
-            $this->set('nav['.$app->getSlug().']', $app->getSlug());
-            if (isset($links[$app->getSlug()])) {
-                $navs = [];
-                foreach ($links[$app->getSlug()] as $page) {
-                   $navs[] = ['link'=>$page];
-                }
-                $this->set('navs['.$app->getSlug().']', $navs);
-            }
-        }
-        
-        
-        return $this->dispatch();
-    }
 
 }
