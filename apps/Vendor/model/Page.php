e2baf4a6350faff3cb3b6b55f41446834f239088
dellpinto <dellpinto@Inspirit>
2016-11-22 22:52:17 +0000

    vendor folder

--- apps/Vendor/model/Page.php
+++ apps/Vendor/model/Page.php
@@ -1,55 +1,264 @@
 <?php
 namespace apps\Vendor\model;
 
-use \model\models\HtmApp;
 use \model\models\Htm;
+use \lib\lang\Language;
+use \model\models\HtmPage;
+use \model\models\HtmTxt;
 
 /**
  * Description of Page
  *
  * @author Luís Pinto / luis.nestesitio@gmail.com
- * Created @Jan 8, 2015
+ * Created @Aug 16, 2016
  */
-class Page extends \model\models\HtmPage {
+class Page {
+    
+    private $model = [];
+
+    private $id;
+    private $stat;
+    private $ord;
+    private $app;
+
+    
 
     /**
-     *
+     * 
+     * @param \model\models\Htm $model
+     * @return \lib\page\Page
      */
-    function initialize() {
-        $this->columnNames[HtmApp::TABLE] = ['slug', 'name'];
-        //$this->columnNames['htm_txt'] = ['desc'];
-        $this->tableJoins[HtmApp::TABLE] = ['join' => 'INNER JOIN', 'left'=>Htm::FIELD_HTM_APP_ID, 'right'=>  HtmApp::FIELD_ID];
-        //$this->tableJoins['htm_txt'] = ['join' => 'INNER JOIN', 'left'=>'htm_txt.htm_page_id', 'right'=>'htm_page.id'];
-        unset($this->tableJoins['langs']);
+    public static function initialize($model){
+        $page = new Page($model);
+        return $page;
+    }
+    
+    /**
+     * 
+     * @param \model\models\Htm $model
+     */
+    function __construct(Htm $model) {
+        $this->id = $model->getId();
+        $this->model['id'] = $this->id;
+        $this->stat = $model->getStat();
+        $this->model['stat'] = $this->stat;
+        $this->ord = $model->getOrd();
+        $this->model['ord'] = $this->ord;
+        $this->app = $model->getHtmApp()->getSlug();
+        $this->model['app'] = $this->app;
+        
+        $this->setLangs();
+        $this->setText();
+        $this->setMedia($model);
+        $this->setVarValue($model);
+        $this->model['page-url'] = $this->getUrl();
+    }
+    
+    public function getUrl(){
+        return \lib\url\UrlHref::renderPageLink($this->app, $this->slug) . '.htm';
+    }
+    
+    /**
+     * 
+     * @return integer
+     */
+    public function getId(){
+        return $this->id;
+    }
+    
+    /**
+     * 
+     * @return string
+     */
+    public function getStat(){
+        return $this->stat;
+    }
+    
+    /**
+     * 
+     * @return integer
+     */
+    public function getOrd(){
+        return $this->ord;
+    }
+    
+    /**
+     * 
+     * @return string
+     */
+    public function getApp(){
+        return $this->app;
+    }
+    
+    private $langmenu = [];
+    private $page = null;
+    
+    /**
+     * 
+     */
+    public function setLangs(){
+         
+        $pages = \model\querys\HtmPageQuery::start()->filterByHtmId($this->id)
+                ->orderByFilter(HtmPage::FIELD_LANGS_TLD, Language::getTldArray())
+                ->find();
+        foreach ($pages as $page){
+            if($this->page == null){
+                $this->page = $page->getId();
+                $this->model['page'] = $this->page;
+                $this->setLangData($page);
+            }
+            
+            $this->langmenu[$page->getLangsTld()] = 
+                    ['app'=>  $this->app, 'slug'=> $page->getSlug(), 'tld'=>$page->getLangsTld()];
+        }
         
     }
-
+    
+    private $lang;
+    private $title;
+    private $slug;
+    private $menu;
+    private $heading;
+    private $updated;
+    
     /**
-     * @param $value
+     * 
+     * @param HtmPage $page
      */
-    function setHtmAppSlug($value) {
-        $this->setColumnValue(HtmApp::FIELD_SLUG, $value);
+    private function setLangData(HtmPage $page){
+        $this->lang = $page->getLangsTld();
+        $this->model['lang'] = $this->lang;
+        $this->title = $page->getTitle();
+        $this->model['title'] = $this->title;
+        $this->slug = $page->getSlug();
+        $this->model['slug'] = $this->slug;
+        $this->menu = $page->getMenu();
+        $this->model['menu'] = $this->menu;
+        $this->heading = $page->getHeading();
+        $this->model['heading'] = $this->heading;
+        $this->updated = $page->getUpdatedAt();
+        $this->model['updated'] = $this->updated;
     }
-
+    
     /**
-     * @return mixed|null
+     * 
+     * @return string
      */
-    function getHtmAppSlug() {
-        return $this->getColumnValue(HtmApp::FIELD_SLUG);
+    public function getLang(){
+        return $this->lang;
     }
-
+    
     /**
-     * @param $value
+     * 
+     * @return string
      */
-    function setHtmAppName($value) {
-        $this->setColumnValue(HtmApp::FIELD_NAME, $value);
+    public function getTitle(){
+        return $this->title;
     }
-
+    
     /**
-     * @return mixed|null
+     * 
+     * @return string
      */
-    function getHtmAppName() {
-        return $this->getColumnValue(HtmApp::FIELD_NAME);
+    public function getSlug(){
+        return $this->slug;
     }
+    
+    /**
+     * 
+     * @return date
+     */
+    public function getUpdated(){
+        return $this->updated;
+    }
+    
+    public function getHeading(){
+        return $this->heading;
+    }
+    
+    /**
+     * 
+     * @return array
+     */
+    public function getLangMenu(){
+        return $this->langmenu;
+    }
+    
+    /**
+     * 
+     * @return array
+     */
+    public function getToArray(){
+        return $this->model;
+    }
+    
+    
+    private $txt = '';
+    private $obs = '';
+    private $footer = '';
+    
+    public function setText(){
+        $this->model[HtmTxt::FIELD_TXT] = $this->txt = '';
+        $text = \model\querys\HtmTxtQuery::start()->filterByHtmPageId($this->page)->findOne();
+        if($text != false){
+            $this->model[HtmTxt::FIELD_TXT] = $this->txt = $text->getTxt();
+        }
+        
+    }
+    
+    
+    public function getTxt(){
+        return $this->txt;
+    }
+    
+    public function getObs(){
+        return $this->obs;
+    }
+    
+    public function getFooter(){
+        return $this->footer;
+    }
+    
+    private $mediasrc = null;
+    
+    public function setMedia(Htm $model){
+        $media = $model->getHtmHasMedia()->getMedia();
+        $src = $media->getSource();
+        if($src != null){
+           $this->mediasrc = $src;
+           $this->model['media-src'] = $this->mediasrc;
+        }
+        
+    }
+    
+    public function setMediaSrc($src){
+        $this->mediasrc = $src;
+        $this->model['media-src'] = $this->mediasrc;
+    }
+    
+    public function getMediaSrc(){
+        return $this->mediasrc;
+    }
+    
+    /*vars*/
+    public function setVarValue(Htm $model){
+        $htmvar = $model->getHtmHasVars()->getHtmVars();
+        $value = $htmvar->getValue();
+        if($value != null){
+            $this->model['var-value'] = $value;
+            $this->model['var'] = $htmvar->getVar();
+        }
+    }
+    
+    /**
+     * 
+     * @param string $column
+     * @param mixed $value
+     */
+    public function setColumnValue($column, $value){
+        $this->model[$column] = $value;
+    }
+    
+    
 
 }
